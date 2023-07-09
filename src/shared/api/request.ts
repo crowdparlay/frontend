1e72d9e71fc124a28946253ea462cfbc496839c4
mere1y <4-mere1y@users.noreply.gitlab.otter.su>
2023-07-10 00:59:27 +0600

    feat(api): add localApi instance, request wrapper

---
+++ src/shared/api/request.ts
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import {createEffect} from 'effector';
+
+import {API_URL} from '../config';
+
+export const localApi = axios.create({
+  baseURL: API_URL,
+  withCredentials: true,
+  validateStatus: (status) => status >= 200 && status < 300,
+});
+
+interface Request {
+  path: string;
+  method: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
+  body?: unknown;
+  params?: unknown;
+}
+
+export const requestFx = createEffect<Request, any>((request) => {
+  return localApi({
+    method: request.method,
+    url: request.path,
+    data: request.body,
+    params: request.params,
+  })
+    .then((response) => response.data)
+    .catch((response) => Promise.reject(response.response.data));
+});
