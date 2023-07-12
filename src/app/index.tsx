56b84bcd35538a476eee76778d9946c5a61c97ff
mere1y <4-mere1y@users.noreply.gitlab.otter.su>
2023-07-12 14:31:37 +0600

    feat(app): add router provider

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,15 @@
+import {RouterProvider} from 'atomic-router-react';
+
 import {Pages} from '~/pages';
+
+import {router} from '~/shared/routes';
 
 import './styles/index.scss';
 
 export const App = () => {
   return (
-    <>
+    <RouterProvider router={router}>
       <Pages />
-    </>
+    </RouterProvider>
   );
 };
