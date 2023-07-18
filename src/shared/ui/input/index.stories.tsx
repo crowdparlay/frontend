534d4946d19565760ead270cc757de93d4c56cea
mere1y <4-mere1y@users.noreply.gitlab.otter.su>
2023-07-18 23:30:12 +0600

    feat(input): add invalid icon, tooltip, styles, stories

--- src/shared/ui/input/index.stories.tsx
+++ src/shared/ui/input/index.stories.tsx
@@ -2,10 +2,13 @@ import type {Meta, StoryObj} from '@storybook/react';
 
 import {Input} from '~/shared/ui';
 
+import {PaddingDecorator} from '../../decorators';
+
 const meta: Meta<typeof Input> = {
   title: 'shared/Input',
   component: Input,
   tags: ['autodocs'],
+  decorators: [PaddingDecorator(40, 40)],
 };
 
 export default meta;
@@ -49,10 +52,21 @@ export const WithValue: Story = {
   },
 };
 
-export const HoverWithValue: Story = {
+export const Invalid: Story = {
   args: {
-    hover: true,
     placeholder: 'Email',
-    defaultValue: 'mere1y@split-team.com',
+    defaultValue: 'mere1y@',
+    isInvalid: true,
+    errorMessage: 'Invalid mail provider',
+  },
+};
+
+export const PasswordInvalid: Story = {
+  args: {
+    placeholder: 'Email',
+    defaultValue: 'toor',
+    type: 'password',
+    isInvalid: true,
+    errorMessage: 'Password must be unique',
   },
 };
--- src/shared/ui/input/index.tsx
+++ src/shared/ui/input/index.tsx
@@ -1,11 +1,15 @@
 import classNames from 'classnames';
 import {InputHTMLAttributes, useCallback, useState} from 'react';
 
+import {Text, TextSize} from '~/shared/ui';
+
+import ErrorIcon from './assets/error.svg';
 import EyeIcon from './assets/eye.svg';
 import cls from './index.module.scss';
 
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   isInvalid?: boolean;
+  errorMessage?: string;
   center?: boolean;
   focus?: boolean;
   hover?: boolean;
@@ -20,6 +24,7 @@ export const Input = (props: InputProps) => {
     focus,
     disabled,
     isInvalid,
+    errorMessage,
     center,
     type = 'text',
     ...otherProps
@@ -30,17 +35,24 @@ export const Input = (props: InputProps) => {
   const customTypeInitial = isPasswordMode && alwaysShown ? 'text' : type;
   const [customType, setCustomType] = useState(customTypeInitial);
 
+  const [isErrorIconHovered, setIsErrorIconHovered] = useState(false);
+
   const mods = {
     [cls.disabled]: disabled,
     [cls.invalid]: isInvalid,
     [cls.center]: center,
-    [cls.focus]: focus,
-    [cls.hover]: hover,
-    [cls.password]: isPasswordMode,
+    [cls.focus]: forceFocus,
+    [cls.hover]: forceHover,
+    [cls.showMods]: isPasswordMode || isInvalid,
+    [cls.showMods2]: isPasswordMode && isInvalid,
   };
 
   const passwordMods = {
     [cls.customType]: customType !== type,
+  };
+
+  const tooltipMods = {
+    [cls.tooltipVisible]: isErrorIconHovered,
   };
 
   const onPasswordButtonClick = useCallback(() => {
@@ -60,15 +72,33 @@ export const Input = (props: InputProps) => {
         {...otherProps}
       />
 
-      {isPasswordMode && (
-        <button
-          type={'button'}
-          className={classNames(cls.passwordButton, passwordMods)}
-          onClick={onPasswordButtonClick}
-        >
-          <EyeIcon />
-        </button>
-      )}
+      <div className={cls.mods}>
+        {isPasswordMode && (
+          <button
+            type={'button'}
+            className={classNames(cls.passwordButton, passwordMods)}
+            onClick={onPasswordButtonClick}
+          >
+            <EyeIcon />
+          </button>
+        )}
+
+        {isInvalid && (
+          <div
+            className={cls.errorContainer}
+            onMouseEnter={() => setIsErrorIconHovered(true)}
+            onMouseLeave={() => setIsErrorIconHovered(false)}
+          >
+            <ErrorIcon />
+
+            {errorMessage && (
+              <Text size={TextSize.S} className={classNames(cls.tooltip, tooltipMods)}>
+                {errorMessage}
+              </Text>
+            )}
+          </div>
+        )}
+      </div>
     </div>
   );
 };
