747e5b58f62c1d6d08b801695381be14c3c1e1de
undrcrxwn <undrcrxwn@gmail.com>
2023-07-25 20:09:38 +0300

    feat(thread): add expanded replies

--- src/widgets/thread/index.stories.tsx
+++ src/widgets/thread/index.stories.tsx
@@ -1,29 +1,29 @@
 import {Meta, StoryObj} from '@storybook/react';
 
-import {Post} from '.';
+import {Thread} from '.';
 
-const meta: Meta<typeof Post> = {
-  title: 'widgets/Post',
-  component: Post,
+const meta: Meta<typeof Thread> = {
+  title: 'widgets/Thread',
+  component: Thread,
   tags: ['autodocs'],
 };
 
 export default meta;
-type Story = StoryObj<typeof Post>;
+type Story = StoryObj<typeof Thread>;
 
 export const Default: Story = {
   args: {
     content: 'Тут по условию задачи нужно посчитать непрерывную серию нулей? Или просто количество нулей после минималки, даже если эта минималка повторяется',
-    replyAvatarUrls: [
-      'https://picsum.photos/70',
-      'https://picsum.photos/80',
-      'https://picsum.photos/90',
-    ],
     author: {
       username: 'crowdparlay',
       displayName: 'Crowd Parlay',
       avatarUrl: 'https://gitlab.otter.su/uploads/-/system/group/avatar/6/crowdparlay_fill.png?width=64',
       verified: true,
-    }
+    },
+    replyAvatarUrls: [
+      'https://picsum.photos/70',
+      'https://picsum.photos/80',
+      'https://picsum.photos/90',
+    ],
   },
 };
---
+++ src/widgets/thread/index.tsx
@@ -0,0 +1,53 @@
+import cls from "~/widgets/thread/index.module.scss";
+import inlineAvatarsCls from "~/widgets/inline-avatars/index.module.scss";
+import classNames from "classnames";
+import {Text, TextSize} from "~/shared/ui";
+import {HTMLAttributes, useState} from "react";
+import {ProfilePreview, ProfilePreviewProps} from "~/widgets/profile-preview";
+import {InlineAvatars} from "~/widgets/inline-avatars";
+
+export interface ThreadProps extends HTMLAttributes<HTMLDivElement> {
+  author?: ProfilePreviewProps;
+  replyAvatarUrls: string[];
+}
+
+export const Thread = (props: ThreadProps) => {
+  const {author, replyAvatarUrls, content, className, ...otherProps} = props;
+
+  const [repliesExpanded, setRepliesExpanded] = useState(false);
+  const [replyThreads, setReplyThreads] = useState<ThreadProps[]>([]);
+
+  const expandReplies = () => {
+    // Fetch replies
+    // setReplyThreads(replies)
+
+    setRepliesExpanded(true);
+  }
+
+  return (
+    <div className={classNames(cls.thread, className)} {...otherProps}>
+      <div className={cls.post}>
+        <div className={cls.content}>
+          <Text size={TextSize.M}>{content}</Text>
+          <ProfilePreview {...author}/>
+        </div>
+        {
+          !repliesExpanded &&
+          <div className={cls.epilogue} onClick={expandReplies}>
+            <InlineAvatars className={inlineAvatarsCls.avatars} avatarUrls={replyAvatarUrls}/>
+            <Text size={TextSize.S}>{replyAvatarUrls.length} {replyAvatarUrls.length === 1 ? 'reply' : 'replies'}</Text>
+          </div>
+        }
+      </div>
+      {
+        repliesExpanded &&
+        replyThreads.map(replyThread =>
+          <div>
+            <div className={cls.connector}/>
+            <Thread {...replyThread}/>
+          </div>
+        )
+      }
+    </div>
+  );
+};
