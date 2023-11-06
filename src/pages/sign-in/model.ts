73b9d0717c322a333d2070f3fa92cf82834548c8
undrcrxwn <undrcrxwn@gmail.com>
2023-11-06 16:20:03 +0300

    fix(sign-in): save access token to local storage

--- src/pages/sign-in/model.ts
+++ src/pages/sign-in/model.ts
@@ -2,8 +2,12 @@ import {attach} from 'effector';
 import {sample} from 'effector';
 import {createForm} from 'effector-forms';
 import {or} from 'patronum';
+import {decodeToken} from 'react-jwt';
 
 import * as api from '~/shared/api';
+import {ApiV1UsersUserIdGet} from '~/shared/api';
+import {JwtPayload} from '~/shared/api/types';
+import {LOCAL_STORAGE_ACCESS_TOKEN_KEY} from '~/shared/config';
 import {routes} from '~/shared/routes';
 import {rules} from '~/shared/rules';
 import {chainAnonymous, sessionRequestFx} from '~/shared/session';
@@ -44,5 +48,22 @@ sample({
 
 sample({
   clock: signInFx.doneData,
+  fn: (data: any): ApiV1UsersUserIdGet => {
+    let userId: string = '';
+
+    if (data.access_token) {
+      localStorage.setItem(LOCAL_STORAGE_ACCESS_TOKEN_KEY, data.access_token);
+      const jwtPayload = decodeToken<JwtPayload>(data.access_token);
+      if (jwtPayload) {
+        userId = jwtPayload.sub;
+      }
+    }
+
+    return {
+      path: {
+        userId,
+      },
+    };
+  },
   target: sessionRequestFx,
 });
