c03c32d4763a2b95949c63e8187ca5884d1933da
merely04 <44271343+merely04@users.noreply.github.com>
2024-01-24 19:20:34 +0100

    feat(post): add base implementation, styles, story

---
+++ src/widgets/post/index.stories.tsx
@@ -0,0 +1,150 @@
+import {Meta, StoryObj} from '@storybook/react';
+
+import {Post} from '.';
+
+const meta: Meta<typeof Post> = {
+  title: 'widgets/Post',
+  component: Post,
+  tags: ['autodocs'],
+};
+
+export default meta;
+type Story = StoryObj<typeof Post>;
+
+const POSTS = [
+  {
+    id: '1',
+    replyId: '-1',
+    author: {
+      id: '1',
+      username: 'mere1y',
+      displayName: 'mere1y',
+      avatarUrl: 'https://avatars.githubusercontent.com/u/44271343?v=4',
+    },
+    date: new Date(),
+    text: (
+      <>
+        вапо ыа вп вы аа вв ыых авпо эыж ваопы длва пды в @sdfgsdfgfsgdf ыдп рыа врп выдл арп а
+        ыдлврап лыорв алпырв лаопр ыдлвао рпдлывора длрвыд алпрылаоп двыраоплырпадлвыорпдлырвдл рр
+        лры вдал рпдылвр аопыдвл рпы лароп ылврпао!
+      </>
+    ),
+    commentators: [
+      {
+        id: '1',
+        username: 'zendet',
+        displayName: 'Z E N D E T',
+        avatarUrl: 'https://source.unsplash.com/random/100x100?sig=2',
+      },
+      {
+        id: '2',
+        username: 'crowdparlay',
+        displayName: 'Crowd Parlay',
+      },
+    ],
+    commentsCount: 2,
+    canReport: true,
+    canReply: true,
+    react: 'like',
+  },
+  {
+    id: '3',
+    replyId: '-1',
+    author: {
+      id: '2',
+      username: 'Imran',
+      displayName: 'Imran',
+      avatarUrl: 'https://source.unsplash.com/random/100x100?sig=2',
+    },
+    date: new Date(),
+    text: <>Подвал</>,
+    commentators: [
+      {
+        id: '1',
+        username: 'zendet',
+        displayName: 'Z E N D E T',
+        avatarUrl: 'https://source.unsplash.com/random/100x100?sig=2',
+      },
+      {
+        id: '2',
+        username: 'crowdparlay',
+        displayName: 'Crowd Parlay',
+      },
+    ],
+    commentsCount: 2,
+    canReport: true,
+    canReply: true,
+    react: 'like',
+  },
+  {
+    id: '4',
+    replyId: '-1',
+    author: {
+      id: '3',
+      username: 'crowdparlay',
+      displayName: 'Crowd Parlay',
+    },
+    date: new Date(),
+    text: <>А всё, конец</>,
+    commentators: [
+      {
+        id: '1',
+        username: 'zendet',
+        displayName: 'Z E N D E T',
+        avatarUrl: 'https://source.unsplash.com/random/100x100?sig=2',
+      },
+      {
+        id: '2',
+        username: 'crowdparlay',
+        displayName: 'Crowd Parlay',
+      },
+    ],
+    commentsCount: 2,
+    canReport: true,
+    canReply: true,
+    react: 'like',
+  },
+];
+
+const renderPosts = (posts: typeof POSTS) => {
+  return posts.map((post) => {
+    const nextPosts = posts.slice(1, posts.length);
+
+    return (
+      // @ts-ignore
+      <Post
+        {...post}
+        commentators={nextPosts.map((p) => p.author)}
+        commentsCount={nextPosts.length}
+        canReply={Math.random() < 0.5}
+        canReport={Math.random() < 0.5}
+        children={nextPosts.length > 0 ? renderPosts(nextPosts) : undefined}
+      />
+    );
+  });
+};
+
+export const Default: Story = {
+  args: {
+    id: '1',
+    author: {
+      id: '1',
+      username: 'mere1y',
+      displayName: 'mere1y',
+      avatarUrl: 'https://avatars.githubusercontent.com/u/44271343?v=4',
+    },
+    date: new Date(),
+    text: (
+      <>
+        вапо ыа вп вы аа вв ыых авпо эыж ваопы длва пды в @sdfgsdfgfsgdf ыдп рыа врп выдл арп а
+        ыдлврап лыорв алпырв лаопр ыдлвао рпдлывора длрвыд алпрылаоп двыраоплырпадлвыорпдлырвдл рр
+        лры вдал рпдылвр аопыдвл рпы лароп ылврпао!
+      </>
+    ),
+    commentators: POSTS.map((p) => p.author),
+    commentsCount: POSTS.length,
+    canReport: true,
+    canReply: true,
+    children: renderPosts(POSTS),
+  },
+};
---
+++ src/widgets/post/index.tsx
@@ -0,0 +1,138 @@
+import classNames from 'classnames';
+import {HTMLAttributes, ReactNode, useCallback, useState} from 'react';
+
+import {InlineAvatars} from '~/features/inline-avatars';
+import {ProfilePreview} from '~/features/profile-preview';
+
+import '~/shared/api';
+import {User} from '~/shared/api/types';
+import {Button, ButtonVariant, Text, TextSize} from '~/shared/ui';
+
+import LikeFillIcon from './icons/likeFill.svg';
+import LikeOutlineIcon from './icons/likeOutline.svg';
+import ReplyIcon from './icons/reply.svg';
+import ReportIcon from './icons/report.svg';
+import cls from './index.module.scss';
+
+export interface Author extends User {
+  avatarUrl?: string;
+}
+
+export interface PostProps extends HTMLAttributes<HTMLDivElement> {
+  id: string;
+  replyId?: string;
+  author: Author;
+  date: Date;
+  text: ReactNode;
+  commentators: Author[];
+  commentsCount: number;
+  canReply: boolean;
+  canReport: boolean;
+  react?: 'like' | 'dislike';
+}
+
+export const Post = (props: PostProps) => {
+  const {
+    id,
+    replyId,
+    author,
+    date,
+    text,
+    commentators,
+    commentsCount,
+    canReply,
+    canReport,
+    react,
+    className,
+    children,
+    ...otherProps
+  } = props;
+
+  const [collapsed, setCollapsed] = useState(true);
+
+  const isReply = Boolean(replyId);
+
+  const onButtonClick = useCallback(() => {
+    setCollapsed((prev) => !prev);
+  }, []);
+
+  return (
+    <div className={classNames(isReply && cls.replyContainer)}>
+      {isReply && <div className={cls.circle} />}
+
+      <div style={{width: '100%'}}>
+        <div data-id={id} className={classNames(cls.post, className)} {...otherProps}>
+          <ProfilePreview
+            username={author.username}
+            displayName={author.displayName}
+            avatarUrl={author.avatarUrl}
+            date={date}
+          />
+
+          <Text size={TextSize.M}>{text}</Text>
+
+          <div className={cls.actions}>
+            <Button
+              onClick={onButtonClick}
+              className={classNames(
+                cls.replyButton,
+                !collapsed && cls.collapse,
+                commentsCount === 0 && cls.hidden,
+              )}
+              variant={ButtonVariant.SECONDARY}
+            >
+              {collapsed ? (
+                <>
+                  <InlineAvatars className={cls.avatars} users={commentators} />
+                  <Text size={TextSize.S}>
+                    {commentsCount} {commentsCount > 1 ? 'replies' : 'reply'}
+                  </Text>
+                </>
+              ) : (
+                <Text size={TextSize.S}>Collapse {commentsCount > 1 ? 'replies' : 'reply'}</Text>
+              )}
+            </Button>
+
+            {canReply && (
+              <Button className={cls.button} variant={ButtonVariant.SECONDARY}>
+                <ReplyIcon />
+                Reply
+              </Button>
+            )}
+            {canReport && (
+              <Button className={cls.button} variant={ButtonVariant.SECONDARY}>
+                <ReportIcon />
+                Report
+              </Button>
+            )}
+
+            <div style={{width: '100%'}} />
+
+            <Button
+              className={classNames(cls.button, cls.react, react === 'like' && cls.active)}
+              variant={ButtonVariant.SECONDARY}
+            >
+              {react === 'like' ? <LikeFillIcon /> : <LikeOutlineIcon />}
+              19
+            </Button>
+            <Button
+              className={classNames(cls.button, cls.react, react === 'dislike' && cls.active)}
+              variant={ButtonVariant.SECONDARY}
+            >
+              {react === 'dislike' ? (
+                <LikeFillIcon className={cls.dislike} />
+              ) : (
+                <LikeOutlineIcon className={cls.dislike} />
+              )}
+              5
+            </Button>
+          </div>
+        </div>
+
+        {children && <div className={cls.line} />}
+
+        {!collapsed && children}
+      </div>
+    </div>
+  );
+};
