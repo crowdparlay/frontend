fdcc188dac0f1099d32bf08e5bd91cf60990b7ef
merely04 <44271343+merely04@users.noreply.github.com>
2024-01-27 09:56:37 +0100

    feat(reply-form): add component, styles, stories

---
+++ src/features/reply-form/index.tsx
@@ -0,0 +1,57 @@
+import classNames from 'classnames';
+import {FormHTMLAttributes, useCallback, useState} from 'react';
+
+import {Button, ButtonVariant, Form, Text, TextArea, TextSize} from '~/shared/ui';
+
+import cls from './index.module.scss';
+
+// @ts-ignore
+export interface ReplyFormProps extends FormHTMLAttributes<HTMLFormElement> {
+  postId: string;
+  maxLength?: number;
+  onSubmit?: (payload: {postId: string; value: string}) => void;
+}
+
+export const ReplyForm = (props: ReplyFormProps) => {
+  const {postId, maxLength, onSubmit, className, ...otherProps} = props;
+
+  const [value, setValue] = useState('');
+
+  const onFormSubmit = useCallback(
+    (value: string) => {
+      setValue('');
+      if (onSubmit !== undefined) {
+        onSubmit({postId, value});
+      }
+    },
+    [postId, onSubmit],
+  );
+
+  return (
+    <Form
+      onSubmit={() => onFormSubmit(value)}
+      className={classNames(cls.container, className)}
+      {...otherProps}
+    >
+      <TextArea
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Введите, вставьте, вырежьте текст здесь..."
+        maxLength={maxLength}
+      />
+
+      <div className={cls.actions}>
+        <Button type="submit">Send</Button>
+
+        <Button variant={ButtonVariant.ACTION}>Insert media</Button>
+        <Button variant={ButtonVariant.ACTION}>Delete</Button>
+
+        <div style={{width: '100%'}} />
+
+        <Text style={{whiteSpace: 'nowrap'}} size={TextSize.S}>
+          {value.length} / {maxLength}
+        </Text>
+      </div>
+    </Form>
+  );
+};
